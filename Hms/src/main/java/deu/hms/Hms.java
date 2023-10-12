518ce348e0545f9093dc8489196eba7f145ea172
bennjung <jss4396@gmail.com>
2023-10-12 21:39:40 +0900

    [INITIAL] Add HMS login feature test1

---
+++ Hms/src/main/java/deu/hms/Hms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.hms;
+
+/**
+ *
+ * @author bennyjung
+ */
+public class Hms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
